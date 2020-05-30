d9adb2db71e02ff5f0bb24a2028af8e45f5cba66
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-05-30 19:30:58 +0300

    'addedHtmlMinMod'

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
 
 
-gulp.task('scss', function() {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
-});
+function html() {
+    return src('dist/index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('app/'));
+}
+
+function css() {
+
+}
+
+exports.html = html;
