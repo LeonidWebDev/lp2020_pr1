aaf918b998b65d6b70dac89fb065639bea9c1885
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-06-01 17:29:37 +0300

    'fullGulpPack'

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const { src, dest } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const htmlmin = require('gulp-htmlmin');
 const del = require('del');
+const sync = require('browser-sync').create();
 
 
 function html() {
@@ -12,7 +13,29 @@ function html() {
 }
 
 function css() {
+    return src('dist/scss/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(dest('app/css/'));
+}
+
+function clean() {
+    return del(['app/index.html', 'app/css/**/*.css'])
+}
+
+function serve() {
+    sync.init({
+        server: './app'
+    })
+
+    watch('app/index.html', series(html)).on('change', sync.reload)
+    watch('dist/scss/style.scss', series(css)).on('change', sync.reload)
 
 }
 
 exports.html = html;
+exports.css = css;
+exports.clean = clean;
+
+exports.build = series(clean, html, css);
+exports.serve = series(clean, html, css, serve);
